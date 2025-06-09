ac917400b568a8b7d1500c0fbc990a04a5a99f15
prathibhaM <prathibhamamindla2910@gmail.com>
2025-06-09 10:19:31 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -34,19 +34,7 @@ const App = () => {
   return (
     <div className="basic-container">
       <div className="basic-heart-center">
-        <svg
-          className="basic-beating-heart"
-          viewBox="0 0 48 48"
-          width="90"
-          height="90"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M24 43s-1.74-1.62-4.55-4.13C10.4 31.36 4 25.28 4 18.5 4 12.7 8.7 8 14.5 8c3.54 0 6.54 2.07 8 5.09C24.96 10.07 27.96 8 31.5 8 37.3 8 42 12.7 42 18.5c0 6.78-6.4 12.86-15.45 20.37C25.74 41.38 24 43 24 43z"
-            fill="#ff2d55"
-          />
-        </svg>
+        <img src="https://icons.iconarchive.com/icons/succodesign/love-is-in-the-web/512/heart-icon.png" alt="Heart" class="beating-heart" />
       </div>
       <div className="basic-formBox">
         <h1>💌 Memories with Dad... </h1>
